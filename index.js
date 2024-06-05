bbae318eabb405703ff090f94fe8aa9c02bcf645
Ahmed KHABER <Ahmed.gsm1983@gmail.com>
2024-06-05 14:13:31 +0100

    commit

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const WebSocket = require('ws');
 
-
 const wss = new WebSocket.Server({ port: 81 });
 
-
 wss.on('connection', function connection(ws) {
-
-    console.log('index.js -->Client connected');
-
+    console.log('index.js -->onConnection:');
 
     ws.on('message', function incoming(message) {
+        // Handle incoming message
+        console.log('index.js -->onMessage: %s', message);
+        ws.send(`Message from websocket server: ${message}, thank you!!`);
 
-        console.log('index.js -->Received: %s', message);
-
-        ws.send(`${message}`);
+        // Broadcast the message to all clients
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send("wss.clients.forEach: " + message);
+            }
+        });
     });
 
-
     ws.on('close', function () {
-        console.log('index.js -->Client disconnected');
+        // Handle connection close
+        console.log('index.js -->inClose:');
     });
 });
